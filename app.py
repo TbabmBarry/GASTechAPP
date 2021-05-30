6c2d23f2390bcf82d769aac1d31bcb06722cbd09
TbabmBarry <liuchicheng0401@hotmail.com>
2021-05-31 00:02:54 +0200

    add fetch heatmap api

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from core.db_config import SQLALCHEMY_DATABASE_URI
 from utils.path_parser import routeParser
+from utils.freq_hour_parser import freqByHourParser
 from dateutil.parser import parse
 from datetime import datetime
 
@@ -58,18 +59,23 @@ def fetch_gps():
     feature_collection = routeParser(gps_paths)
     return jsonify(feature_collection)
 
-@app.route("/fetch_card", methods=['GET','POST'])
-def fetch_card()
-    time_start = request.args.get("time_start")
-    time_end = request.args.get("time_end")
-    time_start = datetime.strptime(time_start, "%y-%m-%d %H:%M")
-    time_end = datetime.strptime(time_end, "%y-%m-%d %H:%M")
-    engine = create_engine(db_conn)
-    Base = automap_base()
-    Base.prepare(engine, reflect = True)
-    db = sessionmaker(bind = engine)()
-    CC = Base.classes.cc_data
-    LOYALTY = Base.classes.loyalty
+# @app.route("/fetch_card", methods=['GET','POST'])
+# def fetch_card()
+#     time_start = request.args.get("time_start")
+#     time_end = request.args.get("time_end")
+#     time_start = datetime.strptime(time_start, "%y-%m-%d %H:%M")
+#     time_end = datetime.strptime(time_end, "%y-%m-%d %H:%M")
+#     engine = create_engine(db_conn)
+#     Base = automap_base()
+#     Base.prepare(engine, reflect = True)
+#     db = sessionmaker(bind = engine)()
+#     CC = Base.classes.cc_data
+#     LOYALTY = Base.classes.loyalty
+
+@app.route("/fetch_heatmap", methods=["GET"])
+def fetch_heatmap():
+    cc_df = pd.read_csv("./static/cc_data.csv")
+    return freqByHourParser(cc_df)
 
 if __name__ == '__main__':
     app.run()
