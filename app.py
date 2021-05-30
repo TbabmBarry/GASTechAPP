24038c3f27c57f861a7b27b862e4d8f500bb1e53
TbabmBarry <liuchicheng0401@hotmail.com>
2021-05-30 16:40:32 +0200

    update backend

--- app.py
+++ app.py
@@ -58,5 +58,18 @@ def fetch_gps():
     feature_collection = routeParser(gps_paths)
     return jsonify(feature_collection)
 
+@app.route("/fetch_card", methods=['GET','POST'])
+def fetch_card()
+    time_start = request.args.get("time_start")
+    time_end = request.args.get("time_end")
+    time_start = datetime.strptime(time_start, "%y-%m-%d %H:%M")
+    time_end = datetime.strptime(time_end, "%y-%m-%d %H:%M")
+    engine = create_engine(db_conn)
+    Base = automap_base()
+    Base.prepare(engine, reflect = True)
+    db = sessionmaker(bind = engine)()
+    CC = Base.classes.cc_data
+    LOYALTY = Base.classes.loyalty
+
 if __name__ == '__main__':
     app.run()
