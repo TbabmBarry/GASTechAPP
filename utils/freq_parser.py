fea2d09d296caf43617176576d5d93c3e73a5879
TbabmBarry <liuchicheng0401@hotmail.com>
2021-06-01 15:59:52 +0200

    return max freq when fetching heatmap data

--- utils/freq_parser.py
+++ utils/freq_parser.py
@@ -5,6 +5,7 @@ def freqByHourParser(df):
     df.timestamp = pd.to_datetime(df.timestamp)
     df.timestamp = df.timestamp.dt.strftime('%H').astype(int)
     freq = df.groupby(['timestamp', 'location']).size().reset_index(name='counts')
+    max_val = int(freq["counts"].max())
     loca_list = list(df.groupby('location').groups.keys())
     loca_dict = dict()
     for i in range(len(loca_list)):
@@ -12,12 +13,13 @@ def freqByHourParser(df):
     res = list()
     for index, row in freq.iterrows():
         res.append([row.timestamp, loca_dict[row.location], row.counts])
-    return [res, loca_list]
+    return [res, loca_list, max_val]
 
 def freqByDayParser(df):
     df.timestamp = pd.to_datetime(df.timestamp)
     df.timestamp = df.timestamp.dt.dayofweek
     freq = df.groupby(['timestamp', 'location']).size().reset_index(name='counts')
+    max_val = int(freq["counts"].max())
     loca_list = list(df.groupby('location').groups.keys())
     loca_dict = dict()
     for i in range(len(loca_list)):
@@ -25,4 +27,4 @@ def freqByDayParser(df):
     res = list()
     for index, row in freq.iterrows():
         res.append([row.timestamp, loca_dict[row.location], row.counts])
-    return [res, loca_list]
+    return [res, loca_list, max_val]
